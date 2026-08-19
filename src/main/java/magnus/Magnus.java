1e5547dde170a8017b344527c28e0043ed79084e
Nguyen-Phuc-Thang <nguyen.phuc.thang.vn@gmail.com>
2026-08-19 21:56:28 +0800

    Add router to Magnus

---
+++ src/main/java/magnus/Magnus.java
@@ -0,0 +1,66 @@
+package magnus;
+
+import java.util.Scanner;
+
+import magnus.command.CommandRouter;
+import magnus.task.Task;
+import magnus.task.TaskList;
+
+public class Magnus {
+    public static void main(String[] args) {
+
+        // Chat decorations
+        String banner = """
+                ███╗   ███╗ █████╗  ██████╗ ███╗   ██╗██╗   ██╗███████╗
+                ████╗ ████║██╔══██╗██╔════╝ ████╗  ██║██║   ██║██╔════╝
+                ██╔████╔██║███████║██║  ███╗██╔██╗ ██║██║   ██║███████╗
+                ██║╚██╔╝██║██╔══██║██║   ██║██║╚██╗██║██║   ██║╚════██║
+                ██║ ╚═╝ ██║██║  ██║╚██████╔╝██║ ╚████║╚██████╔╝███████║
+                ╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚══════╝
+                """;
+
+        String divider = "____________________________________________________________\n";
+        String indent = "\t";
+
+        // Greeting and exit text
+        String greeting = """
+                Hello! I'm Magnus.
+                How can I help you today?
+                """;
+        
+        String exit = "Goodbye. See you soon!";
+
+        // Greeting
+        System.out.println(divider);
+        System.out.print(banner);
+        System.out.print(greeting);
+        System.out.println(divider);
+
+        // Chat resources
+        Scanner scanner = new Scanner(System.in);
+        TaskList tasks = new TaskList();
+        CommandRouter router = new CommandRouter(tasks);
+
+        // Chat loop
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            // Start of result
+            System.out.println(indent + divider);
+
+            router.route(userInput);
+
+            if (userInput.equals("bye")) {
+                break;
+            }
+
+            // End of result
+            System.out.println(indent + divider);
+        }
+
+        System.out.println(indent + divider); 
+        
+        // Exit
+        scanner.close();
+    }
+}
